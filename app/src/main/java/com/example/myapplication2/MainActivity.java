64eac27f49d6077f1a642585e3b6ee766cd435f0
yrss <847065177@qq.com>
2020-09-18 15:05:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView out;
+    EditText edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        out=findViewById(R.id.textView);
+        //out.setText("haha");
+
+        edit=findViewById(R.id.editText1);
+        //String str=input.getText().toString();
+
+
+    }
+    public void btn(View v){
+        String str=edit.getText().toString();
+        float t1=Float.parseFloat(str);
+        double t2=t1 * 33.8;
+        String outstr=String.valueOf(t2);
+        out.setText(outstr);
+    }
+
+}
